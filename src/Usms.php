d6fb0d65f103877403ab839c8da851c762c506b1
sefakor20 <sefakorhom2012@gmail.com>
2021-06-15 16:08:44 +0000

    add contact group module

--- src/Usms.php
+++ src/Usms.php
@@ -13,7 +13,7 @@ class UrhitechSMSAPI
      * 
      * Send Request to server and get sms status
      */
-    private function send_server_response($request_method, $url, $api_token, $post_fields = null)
+    private function send_server_response($url, $api_token, $post_fields = null, $request_method = null)
     {
         $ch = curl_init();
 
@@ -25,10 +25,28 @@ class UrhitechSMSAPI
             curl_setopt ($ch, CURLOPT_POST, true);
             curl_setopt ($ch, CURLOPT_POSTFIELDS, $data);
         }
+
+        // request_method == PUT
+        if ($request_method == 'put') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+
+        // request_method == PATCH
+        if ($request_method == 'patch') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+
+        // request_method == DELETE
+        if ($request_method == 'delete') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        }
+
         curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt ($ch, CURLOPT_HTTPHEADER, [
             "accept: application/json",
-            "authorization: Bearer".$api_token
+            "authorization: Bearer ".$api_token
         ]);
 
         // Allow cURL function to execute 20sec
@@ -38,10 +56,11 @@ class UrhitechSMSAPI
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
 
         $response = curl_exec( $ch );
-        if ($e = curl_close( $ch )) {
-            echo $e;
+        if ($e = curl_error($ch)) {
+            return $e;
         } else {
-            return json_encode($response, true) ;
+            return json_decode($response, true);
+            curl_close($ch);
         }
 
     }
@@ -57,7 +76,86 @@ class UrhitechSMSAPI
      */
     public function send_sms($url, $api_token, $post_fields = null)
     {
-        $response = $this->send_server_response('post', $url, $api_token, $post_fields);
+        $response = $this->send_server_response($url, $api_token, $post_fields, 'post');
         return $response;
     }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @param $post_fields => ['name' => 'your group name']
+     * @return mixed
+     * 
+     * Create a new Contact Group
+     */
+    public function create_contact_group($url, $api_token, $post_fields)
+    {
+        $response = $this->send_server_response($url, $api_token, $post_fields, 'post');
+        return $response;
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     * 
+     * View Contact Group
+     */
+    public function view_contact_group($url, $api_token)
+    {
+        $response = $this->send_server_response($url, $api_token, '', 'post');
+        return $response;
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @param $post_fields
+     * @return mixed
+     * 
+     * Update Contact Group
+     */
+    public function update_contact_group($url, $api_token, $post_fields)
+    {
+        $response = $this->send_server_response($url, $api_token, $post_fields, 'patch');
+        return $response;
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     * 
+     * Delete Contact Group
+     */
+    public function delete_contact_group($url, $api_token)
+    {
+        $response = $this->send_server_response($url, $api_token, '', 'delete');
+        return $response;
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     * 
+     * Delete Contact Group
+     */
+    public function all_contact_groups($url, $api_token)
+    {
+        $response = $this->send_server_response($url, $api_token, '', '');
+        return $response;
+    }
+
+
 }

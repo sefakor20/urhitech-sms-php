c7948d7e1a30f88ecf71ee67cd3b4baaba0fadad
sefakor20 <sefakorhom2012@gmail.com>
2021-06-15 05:35:56 +0000

    add send sms & view profile balance

--- src/Usms.php
+++ src/Usms.php
@@ -13,14 +13,13 @@ class UrhitechSMSAPI
      * 
      * Send Request to server and get sms status
      */
-    private function send_server_response($request_method, $url, $api_token, $post_fields = null)
+    private function send_server_response($url, $api_token, $request_method = null, $post_fields = null)
     {
         $ch = curl_init();
 
         $data = json_encode($post_fields);
 
         curl_setopt ($ch, CURLOPT_URL, $url);
-
         if ($request_method == 'post') {
             curl_setopt ($ch, CURLOPT_POST, true);
             curl_setopt ($ch, CURLOPT_POSTFIELDS, $data);
@@ -28,7 +27,7 @@ class UrhitechSMSAPI
         curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt ($ch, CURLOPT_HTTPHEADER, [
             "accept: application/json",
-            "authorization: Bearer".$api_token
+            "authorization: Bearer ".$api_token
         ]);
 
         // Allow cURL function to execute 20sec
@@ -38,10 +37,11 @@ class UrhitechSMSAPI
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
 
         $response = curl_exec( $ch );
-        if ($e = curl_close( $ch )) {
-            echo $e;
+        if ($e = curl_error( $ch )) {
+            return $e;
         } else {
-            return json_encode($response, true) ;
+            return json_decode($response, true) ;
+            curl_close($ch);
         }
 
     }
@@ -53,11 +53,55 @@ class UrhitechSMSAPI
      * @param $post_fields
      * @return mixed
      * 
-     * Send SMS
+     * Send single SMS
      */
-    public function send_sms($url, $api_token, $post_fields = null)
+    public function send_sms($url, $api_token, $post_fields)
     {
-        $response = $this->send_server_response('post', $url, $api_token, $post_fields);
+        $response = $this->send_server_response($url, $api_token, 'post',  $post_fields);
+        return $response;
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @param $post_fields
+     * @return mixed
+     * 
+     * View an SMS
+     */
+    public function view_sms($url, $api_token)
+    {
+        $response = $this->send_server_response($url, $api_token,'', '');
+        return $response;
+    }
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     * 
+     * View profile
+     */
+    public function profile($url, $api_token)
+    {
+        $response = $this->send_server_response($url, $api_token, '', '');
+        return $response;
+    }
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     * 
+     * View sms credit balance
+     */
+    public function check_balance($url, $api_token)
+    {
+        $response = $this->send_server_response($url, $api_token, '','');
         return $response;
     }
 }
